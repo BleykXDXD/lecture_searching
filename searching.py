b70c6aac5a6d333b5d5029f85184c1df9c8ee38a
Zdeněk Konečný <262731@vutbr.cz>
2026-04-16 16:32:25 +0200

    Pokus

--- searching.py
+++ searching.py
@@ -1,5 +1,9 @@
 import os
 import json
+import time
+from random import choices
+
+import matplotlib.pyplot as plt
 # get current working directory path
 cwd_path = os.getcwd()
 
@@ -22,18 +26,21 @@ def read_data(file_name, field):
 def linear_search(field, number):
     seznam = []
     count = 0
+    start = time.perf_counter()
     for idx, value in enumerate(field):
         if value == number:
             seznam.append(idx)
             count += 1
+    end = time.perf_counter()
+
+    duration = end - start
+    return duration
     return {"Positions": seznam, "Count": count}
 
 def binary_search(field, number):
-    middle = len(field)/2
-    number_middle = int(middle)
     left = 0
     right = len(field) - 1
-
+    start = time.perf_counter()
     while left <= right:
         mid = (left + right) // 2
         middle_value = field[mid]
@@ -44,11 +51,21 @@ def binary_search(field, number):
             left = mid + 1
         else:
             return mid
+    end = time.perf_counter()
+
+    return end - start
     return None
 
 
 
 
+def unordered_sequence(max_len=100):
+    """
+    Returns list of unordered ints from within a range between -1000 and 1000.
+    :param max_len: (int) desired length of sequence
+    :return: (list) sequence of numbers
+    """
+    return choices(range(-1000, 1000), k=max_len)
 
 
 
@@ -60,6 +77,7 @@ def main():
     print(sequential_data)
     print(linear_search([54, 2, 18, 5, 3, 31, 20, 65, -10, 300, 17, 5, -1, 0, 0, 102, 7, 8, 9, 9, -3, -5, 0, 1, 63, 82, -36, -5], 9))
     print(binary_search(sequential_data1,13))
+
 
 
 if __name__ == '__main__':
